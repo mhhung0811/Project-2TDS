6ecb516134ae1e2894a2cf39fd579470ee00aefd
Bui Thai Hoang <buithaihoang04gl@gmail.com>
2024-12-31 19:46:07 +0700

    bullet shotgun

---
+++ Assets/Scripts/Factory/Bullet/Type/ShotGunBullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotGunBullet : Projectile
+{
+	public void OnTriggerEnter2D(Collider2D collision)
+	{
+		IEnemyInteractable enemy = collision.gameObject.GetComponent<IEnemyInteractable>();
+		if (enemy != null)
+		{
+			enemy.OnEnemyBulletHit(settings.damage);
+			settings.flyweightEvent.Raise(this);
+			return;
+		}
+
+		if (collision.gameObject.CompareTag("Wall"))
+		{
+			settings.flyweightEvent.Raise(this);
+			return;
+		}
+	}
+}
