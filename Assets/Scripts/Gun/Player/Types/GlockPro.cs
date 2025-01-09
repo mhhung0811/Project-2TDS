6c2c23ad814db8e876fbefcd439a855681ceae71
Bui Thai Hoang <buithaihoang04gl@gmail.com>
2025-01-09 20:26:13 +0700

    Animation, logic gun glock and assault Rifle

---
+++ Assets/Scripts/Gun/Player/Types/GlockPro.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlockPro : GunBase
+{
+	public GunData gunData;
+	public FlyweightTypeVector2FloatEvent takeBulletEvent;
+	public VoidEvent playerShootEvent;
+
+	public override void InitGunData()
+	{
+		gunName = gunData.gunName;
+		maxAmmoPerMag = gunData.maxAmmoPerMag;
+		currentAmmo = gunData.currentAmmo;
+		totalAmmo = gunData.totalAmmo;
+		reloadTime = gunData.reloadTime;
+		fireRate = gunData.fireRate;
+		damage = gunData.damage;
+		bulletSpeed = gunData.bulletSpeed;
+	}
+
+	public override void ResetGunData()
+	{
+		gunName = gunData.gunName;
+		maxAmmoPerMag = gunData.maxAmmoPerMag;
+		currentAmmo = gunData.maxAmmoPerMag;
+		totalAmmo = gunData.totalAmmo;
+		reloadTime = gunData.reloadTime;
+		fireRate = gunData.fireRate;
+		damage = gunData.damage;
+		bulletSpeed = gunData.bulletSpeed;
+	}
+
+
+	public override void Shoot(float angle)
+	{
+		if (!CanShoot()) return;
+
+		takeBulletEvent.Raise((FlyweightType.SniperBullet, new Vector2(this.transform.position.x, this.transform.position.y), angle));
+
+		currentAmmo--;
+		UpdateLastShootTime();
+		StateMachine.ChangeState(ShootingState);
+
+		playerShootEvent.Raise(new Void());
+	}
+}
